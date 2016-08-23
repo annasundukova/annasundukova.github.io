945342089e5feab22d95f3c511d8084beb09191e
Robert Danitz <robert.danitz@posteo.de>
2016-08-23 21:13:36 +0200

    update to 8710c255b776ff3449e2d0b7a9b4cabb4b2a4df3

--- tags/about.js
+++ tags/about.js
@@ -1,4 +1,4 @@
-riot.tag('about', '<div class="pure-g"><div class="pure-u-2-5 about-portrait noselect"><img src="images/me.jpeg"></img></div><div class="pure-u-3-5 about-descr"><h1 class="about-header">CV</h1><div class="about-cv"><raw content="{ me.cv }"></raw></div></div><div class="pure-u-3-4 about-abstract noselect"> about me </div><div class="pure-u-1-4 nav2 noselect"><span><a href="#gallery">^</a></span></div></div>', function(opts) {var self;
+riot.tag('about', '<div class="pure-g"><div class="pure-u-2-5 about-portrait noselect"><img src="images/me.jpg"></img></div><div class="pure-u-3-5 about-descr"><h1 class="about-header">CV</h1><div class="about-cv"><raw content="{ me.cv }"></raw></div></div><div class="pure-u-3-4 about-abstract noselect"> about me </div><div class="pure-u-1-4 nav2 noselect"><span><a href="#gallery">^</a></span></div></div>', function(opts) {var self;
 
 self = this;
 
--- tags/myframe.js
+++ tags/myframe.js
@@ -1,4 +1,4 @@
-riot.tag('myframe', '<div class="frame"><div class="frame-image noselect"><img riot-src="images/{ project.name }/thumb.jpeg" width="100%"></img></div><div class="frame-text"><p><a href="#project/{ project.name }">{ project.abstract}</a></p></div></div>', function(opts) {var self;
+riot.tag('myframe', '<div class="frame"><div class="frame-image noselect"><img riot-src="images/{ project.name }/thumb.jpg" width="100%"></img></div><div class="frame-text"><p><a href="#project/{ project.name }">{ project.abstract}</a></p></div></div>', function(opts) {var self;
 
 self = this;
 
