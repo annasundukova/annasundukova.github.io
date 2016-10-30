d45b62257183e2a2365210e577661dd7b8d06127
Robert Danitz <robert.danitz@posteo.de>
2016-10-30 16:47:14 +0100

    update to 9a45463b33f5885b33323390ca216c6b89097ea5

--- tags/you.js
+++ tags/you.js
@@ -1,3 +1,3 @@
-riot.tag('you', '<div class="frame"><div class="you"><span><a href="{ you.github }" >&copy;{ you.name }</a></span></div></div>', function(opts) {
+riot.tag('you', '<div class="frame"><div class="you"><span><a href="https://github.com/{ you.github }">&copy;{ you.name }</a></span></div></div>', function(opts) {
 
 });
